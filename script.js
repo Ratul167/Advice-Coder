75e0d5cc32382f4539c9357a20d72bdacc9bbb68
Ratul Biswas <98582203+Ratul167@users.noreply.github.com>
2022-12-20 17:46:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,4 @@
 
-
-
-(async () => {
-  const url = 'https://api.adviceslip.com/advice'
-  const response = await fetch(url);
-  let data = await response.json();
-  //  console.log(data);
-  let advice = (data['slip']['advice'])
-  console.log(advice);
-
-
-  document.getElementById("advice").innerHTML = advice;
-})();
-
-
 async function advice() {
   const url = 'https://api.adviceslip.com/advice'
   const response = await fetch(url);
@@ -22,3 +7,5 @@ async function advice() {
   console.log(advice);
   document.getElementById("advice").innerHTML = advice;
 }
+
+advice()
